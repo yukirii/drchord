8b06718413ee77bffe4e2d29578664c55473e76a
shiftky <shiftky@gmail.com>
2013-10-03 11:10:36 +0900

    add lib/node_info.rb

---
+++ lib/node_info.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "zlib"
+
+module DRChord
+  class NodeInfomation
+    attr_reader :ip, :port
+    def initialize(ip, port)
+      @ip = ip
+      @port = port
+    end
+
+    def id
+      return Zlib.crc32("#{@ip}:#{@port}")
+    end
+
+    def uri(arg = "chord")
+      uri = "druby://#{@ip}:#{@port}"
+      case arg
+      when "chord"
+        uri += "?chord"
+      when "dhash"
+        uri += "?dhash"
+      end
+    end
+  end
+end
